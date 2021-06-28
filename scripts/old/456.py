d46859ba5626e97571a582a343d8c7eb576889ba
yarg666 <30553714+yarg666@users.noreply.github.com>
2021-06-28 09:25:32 +0200

    mpm

--- scripts/old/456.py
+++ scripts/old/456.py
@@ -1,7 +1,7 @@
-#import hou 
+import hou 
 
-#desktops_dict = dict((d.name(), d) for d in hou.ui.desktops())
-#desktops_dict['CGEV'].setAsCurrent()
+desktops_dict = dict((d.name(), d) for d in hou.ui.desktops())
+desktops_dict['CGEV'].setAsCurrent()
 
 #cgBuild = obj.createNode("sceneBuilder","sceneBuilder")
 #cgBuild.move(hou.Vector2(5.0,4.0))
